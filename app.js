cc82cbc39954b38f2d9966a755cac35067c9d04e
Khôi Trần Duy <20521483@ms.uit.edu.vn>
2023-03-17 12:42:25 +0700

    connect to database

--- app.js
+++ app.js
@@ -2,6 +2,17 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+//Connect to database
+mongoose.connect(
+    "mongodb+srv://mysocialnetwork:" +
+    process.env.MONGO_PASS +
+    "@social-network-kreat.myvii0l.mongodb.net/?retryWrites=true&w=majority",
+    {
+        useMongoClient: true
+    }
+);
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({
@@ -10,7 +21,9 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 
 app.use('/', (req, res, next) => {
-    res.status(200).send('hello world haha');
+    res.status(200).json({
+        message: 'hello world'
+    });
 })
 
 module.exports = app;
