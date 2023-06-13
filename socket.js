803d346dd0c2f7829e3f3aa0fca8650934e598e8
Khôi Trần Duy <20521483@ms.uit.edu.vn>
2023-06-13 22:07:19 +0700

    sua socket io

--- socket.js
+++ socket.js
@@ -35,12 +35,13 @@ io.on('connection', (socket) => {
     io.to(socket.id).emit('getUser', onlineUsers);
   });
 
-  socket.on('disconnect', () => {
+  socket.on('disconnect', async () => {
+    const disconnectedUser = await onlineUsers.find((user) => user.socketId === socket.id);
     removeOnlineUser(socket.id);
-  });
-
-  socket.on('removeUser', async (id_account) => {
-    const myListFriend = await OtherInfo.findOne({ id_account: id_account }, { _id: 0, listFriend: 1 });
+    const myListFriend = await OtherInfo.findOne(
+      { id_account: disconnectedUser.id_account },
+      { _id: 0, listFriend: 1 },
+    );
     let onlineFriends = onlineUsers.filter((value) => myListFriend.listFriend.includes(value.id_account));
     for (const [index, friend] of onlineFriends.entries()) {
       io.to(friend.socketId).emit('getUser', onlineUsers);
