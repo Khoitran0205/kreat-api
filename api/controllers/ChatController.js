19c44e53ef984a9659f775bdb9d1c6da73ba46e4
Khôi Trần Duy <20521483@ms.uit.edu.vn>
2023-07-16 15:24:01 +0700

    fix bugs

--- api/controllers/ChatController.js
+++ api/controllers/ChatController.js
@@ -68,16 +68,6 @@ exports.chat_get_all_conversation = async (req, res, next) => {
           isYou: latestMessage[0].id_sender == decodedToken.id_account,
           isViewed: latestMessage[0].id_sender == decodedToken.id_account ? true : conversation.isViewed,
         };
-      } else {
-        conversationContent = {
-          id_conversation: conversation._id,
-          avatar: personalInfo.avatar,
-          fullName: personalInfo.fullName,
-          latestMessage: '',
-          latestMessageTime: '',
-          isYou: false,
-          isViewed: true,
-        };
       }
 
       listConversation.push(conversationContent);
