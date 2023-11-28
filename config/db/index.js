ba1bb8f4fabf90ec6ca6e163119f0ae475f77f62
Khoi Tran <duykhoi96tn@gmail.com>
2023-11-28 16:42:45 +0700

    update

--- config/db/index.js
+++ config/db/index.js
@@ -2,14 +2,9 @@ const mongoose = require('mongoose');
 
 const connect = async () => {
   try {
-    const connect = await mongoose.connect(
-      'mongodb+srv://mysocialnetwork:' +
-        process.env.MONGO_PASS +
-        '@social-network-kreat.myvii0l.mongodb.net/?retryWrites=true&w=majority',
-      {
-        dbName: 'KreaT',
-      },
-    );
+    const connect = await mongoose.connect(process.env.MONGO_URL, {
+      dbName: 'KreaT',
+    });
     console.log('Connect to db successfully!');
   } catch (err) {
     console.log('Fail to connect to db!');
