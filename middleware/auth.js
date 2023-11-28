63be796a176bebd906529c0b088992c8e3cdcdec
Khoi Tran <duykhoi96tn@gmail.com>
2023-11-28 21:25:52 +0700

    update require

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,12 +1,10 @@
-const jwt = require('jsonwebtoken');
-
 const authenticateToken = async (req, res, next) => {
   const authHeader = await req.header('Authorization');
   const token = await (authHeader && authHeader.split(' ')[1]);
 
   if (!token) return await res.sendStatus(401);
 
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, async (err, user) => {
+  require('jsonwebtoken').verify(token, process.env.ACCESS_TOKEN_SECRET, async (err, user) => {
     if (err) return await res.sendStatus(403);
     req.user = user;
     await next();
