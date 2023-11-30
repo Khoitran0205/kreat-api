2d18b540be7a7009480575e349a26e6fe3158a86
Khoi Tran <duykhoi96tn@gmail.com>
2023-11-30 22:09:18 +0700

    update

--- socket.js
+++ socket.js
@@ -71,6 +71,7 @@ io.on('connection', (socket) => {
 
   // call other user
   socket.on('callUser', async (id_conversation, id_sender, id_receiver) => {
+    console.log('get call');
     const user = getOnlineUser(id_receiver);
     const senderInfo = await PersonalInfo.findOne({ id_account: id_sender }, { _id: 0, avatar: 1, fullName: 1 });
     io.to(user.socketId).emit('getCall', {
