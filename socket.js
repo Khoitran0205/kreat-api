99dacd5da4cebfb0c56da5c80d85c7e459192edf
Khoi Tran <duykhoi96tn@gmail.com>
2023-12-05 14:43:31 +0700

    update socket

--- socket.js
+++ socket.js
@@ -70,7 +70,7 @@ io.on('connection', (socket) => {
   });
 
   // call other user
-  socket.on('callUser', async ({ id_conversation, id_sender, id_receiver, peerData, peer }) => {
+  socket.on('callUser', async ({ id_conversation, id_sender, id_receiver, peerData }) => {
     const user = getOnlineUser(id_receiver);
     const senderInfo = await PersonalInfo.findOne({ id_account: id_sender }, { _id: 0, avatar: 1, fullName: 1 });
     io.to(user.socketId).emit('getCall', {
@@ -79,12 +79,11 @@ io.on('connection', (socket) => {
       avatar: senderInfo?.avatar,
       fullName: senderInfo?.fullName,
       peerData,
-      peer,
     });
   });
 
   // answer the call
-  socket.on('answerCall', async ({ id_conversation, id_sender, id_receiver, peerData, peer }) => {
+  socket.on('answerCall', async ({ id_conversation, id_sender, id_receiver, peerData }) => {
     const user = getOnlineUser(id_receiver);
     const senderInfo = await PersonalInfo.findOne({ id_account: id_sender }, { _id: 0, avatar: 1, fullName: 1 });
     io.to(user.socketId).emit('callAccepted', {
@@ -93,7 +92,6 @@ io.on('connection', (socket) => {
       avatar: senderInfo?.avatar,
       fullName: senderInfo?.fullName,
       peerData,
-      peer,
     });
   });
 
